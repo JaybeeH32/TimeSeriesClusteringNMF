d1c6d11d722599228e0cab1136070ea849bb4fce
BenjaminisCoding <103667717+BenjaminisCoding@users.noreply.github.com>
2024-01-07 21:17:36 +0100

    Node2Vec added

--- clustering_algos.py
+++ clustering_algos.py
@@ -1,45 +1,27 @@
 from sklearn.cluster import KMeans
 from sklearn.cluster import SpectralClustering
 from sklearn.mixture import GaussianMixture
-import numpy as np
+from randomwalk import Node2Vec
 
-
-def kmeans(A, d, direct=True):
-    model = KMeans(n_clusters=d, n_init='auto')
+def kmeans(A, d):
+    model = KMeans(n_clusters=d)
     model.fit(A)
     predicted_clusters = model.predict(A)
-    if direct:
-        return predicted_clusters
-    else:
-        # artefact to align all clustering methods on nmf 
-        n = len(A)
-        arr = np.zeros(shape=(n, d))
-        for i in range(n):
-            arr[i, predicted_clusters[i]] = 1.0
-        return arr, 0, 0
+    return predicted_clusters
 
-def spectral_clustering(A, d, direct=True):
+def spectral_clustering(A, d):
     model = SpectralClustering(n_clusters=d)
     predicted_clusters = model.fit_predict(A)
-    if direct:
-        return predicted_clusters
-    else:
-        # artefact to align all clustering methods on nmf 
-        n = len(A)
-        arr = np.zeros(shape=(n, d))
-        for i in range(n):
-            arr[i, predicted_clusters[i]] = 1.0
-        return arr, 0, 0
+    return predicted_clusters
 
-def gaussian_mixture(A, d, direct=True):
+def gaussian_mixture(A, d):
     model = GaussianMixture(n_components=d)
     predicted_clusters = model.fit_predict(A)
-    if direct:
-        return predicted_clusters
-    else:
-        # artefact to align all clustering methods on nmf 
-        n = len(A)
-        arr = np.zeros(shape=(n, d))
-        for i in range(n):
-            arr[i, predicted_clusters[i]] = 1.0
-        return arr, 0, 0
+    return predicted_clusters
+
+def node2vec_clustering(G, n_dim, n, length, p, q, d):
+
+    node2vec = Node2Vec(G, n_dim, n, length, p, q)
+    embeddings = node2vec.Skipgram_embeddings()
+    predicted_clusters = kmeans(embeddings, d)
+    return predicted_clusters
